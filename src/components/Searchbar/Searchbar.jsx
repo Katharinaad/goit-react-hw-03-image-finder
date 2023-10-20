ffb701c6ede5139b04f7de5d161af27189ce4013
Katharinaad <vermilion.adams@gmail.com>
2023-10-20 23:48:36 +0200

    Create Searchbar.jsx

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,24 @@
+export function Searchbar({ onSubmit }) {
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    event.currentTarget.reset();
+  };
+
+  return (
+    <header className="searchbar">
+      <form className="form" onSubmit={handleSubmit}>
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </button>
+        <input
+          className="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+}
