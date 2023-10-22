d61511e74543c3ca1ce59524ad10382898b8e2e1
Katharinaad <vermilion.adams@gmail.com>
2023-10-22 17:17:36 +0200

    Update api.js

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -5,5 +5,14 @@ export const fetchPics = async () => {
     'https://pixabay.com/api/?q=cat&page=1&key=38980097-7acd167c88be026b0eb497bb1&image_type=photo&orientation=horizontal&per_page=12';
   const { data } = await axios.get(url);
 
+  console.log(data.hits);
+  return data.hits;
+};
+
+export const findPictureByName = async query => {
+  const url = `https://pixabay.com/api/?q=${query}&page=1&key=38980097-7acd167c88be026b0eb497bb1&image_type=photo&orientation=horizontal&per_page=12`;
+  const { data } = await axios.get(url);
+
+  console.log(data.hits);
   return data.hits;
 };
