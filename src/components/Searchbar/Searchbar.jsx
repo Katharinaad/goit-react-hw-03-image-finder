6991f972dbc09231f4dad76efd0e00119654720d
Katharinaad <vermilion.adams@gmail.com>
2023-10-22 17:17:33 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,24 +1,27 @@
-export function Searchbar({ onSubmit }) {
-  const handleSubmit = event => {
+const { Component } = require('react');
+
+export class Searchbar extends Component {
+  handleSubmit = event => {
     event.preventDefault();
 
     event.currentTarget.reset();
   };
-
-  return (
-    <header className="searchbar">
-      <form className="form" onSubmit={handleSubmit}>
-        <button type="submit" className="button">
-          <span className="button-label">Search</span>
-        </button>
-        <input
-          className="input"
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-        />
-      </form>
-    </header>
-  );
+  render() {
+    return (
+      <header className="searchbar">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+          <input
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
 }
