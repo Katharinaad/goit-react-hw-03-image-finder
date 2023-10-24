04dbd4e78f3c5471cb10c83daf78126b157c2073
Katharinaad <vermilion.adams@gmail.com>
2023-10-25 00:40:26 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,24 +1,38 @@
-const { Component } = require('react');
+import css from './Searchbar.module.css';
+import { Component } from 'react';
 
 export class Searchbar extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  handleChange = event => {
+    this.setState({ searchQuery: event.currentTarget.value });
+  };
+
   handleSubmit = event => {
     event.preventDefault();
+    const query = this.state.searchQuery;
 
-    event.currentTarget.reset();
+    this.props.onSubmit(query);
+    this.setState({ searchQuery: '' });
   };
   render() {
     return (
-      <header className="searchbar">
-        <form className="form" onSubmit={this.handleSubmit}>
-          <button type="submit" className="button">
-            <span className="button-label">Search</span>
+      <header className={css.searchBar}>
+        <form className={css.searchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={css.searchButton}>
+            Search
           </button>
+
           <input
-            className="input"
+            className={css.searchInput}
             type="text"
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
+            onChange={this.handleChange}
+            value={this.state.searchQuery}
           />
         </form>
       </header>
